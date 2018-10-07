a4e5ea79901f7b941a55fbd759e56d2c34fe9bfb
Mateusz <206037@edu.p.lodz.pl>
2018-10-07 05:05:48 +0200

    Lab
    Punkt 3.1.5

--- powp_rhymers_bridge-master/src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ powp_rhymers_bridge-master/src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	private int[] NUMBERS = new int[12];
+	private static final int max_total = -1;
+	private static final int max_numbers = 12;
 
-	public int total = -1;
+	private int[] NUMBERS = new int[max_numbers];
+
+	public int total = max_total;
 
 	public void countIn(int in) {
 		if (!isFull())
@@ -12,7 +15,7 @@ public class DefaultCountingOutRhymer {
 	}
 
 	public boolean callCheck() {
-		return total == -1;
+		return total == max_total;
 	}
 
 	public boolean isFull() {
@@ -21,13 +24,13 @@ public class DefaultCountingOutRhymer {
 
 	protected int peekaboo() {
 		if (callCheck())
-			return -1;
+			return max_total;
 		return NUMBERS[total];
 	}
 
 	public int countOut() {
 		if (callCheck())
-			return -1;
+			return max_total;
 		return NUMBERS[total--];
 	}
 
